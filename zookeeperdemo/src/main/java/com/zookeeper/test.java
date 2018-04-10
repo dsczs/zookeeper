a1544e71d618710aa8d5eb026fe65fe4d484bf43
dsczs <370637594@qq.com>
2018-04-10 21:24:51 +0800

    zookeeper demo

--- zookeeperdemo/src/main/java/com/zookeeper/test.java
+++ zookeeperdemo/src/main/java/com/zookeeper/test.java
@@ -24,5 +24,32 @@ public class test {
             zooKeeper.create("/node","alan".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
         }
         System.out.println(Arrays.asList(zooKeeper.getChildren("/", true)));
+
+        if(zooKeeper.exists("/node",true) != null){
+            zooKeeper.delete("/node",-1);
+        }
+
+        zooKeeper.close();
     }
+
+    @Test
+    public void testQueue() throws IOException, KeeperException, InterruptedException {
+        ZooKeeper zooKeeper = new ZooKeeper("192.168.138.129:2181", 6000, new Watcher() {
+            //监听所以被触发的事件
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                System.out.println("触发了事件 " + watchedEvent.getType());
+            }
+        });
+
+        if (zooKeeper.exists("/queue",false) == null){
+            System.out.println("创建队列queue");
+            zooKeeper.create("/queue","task-queue".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+        }else {
+            System.out.println("队列queue已存在");
+        }
+
+
+    }
+
 }
